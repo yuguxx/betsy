a58feffd10a02d439da1123b840c06dc981e7d2e
yuguxx <146349981+yuguxx@users.noreply.github.com>
2024-08-19 19:35:14 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,12 +6,12 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: false,
+  rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '2skydev blog',
+  name: 'betsy',
   domain: 'blog.2skydev.com',
-  author: '2skydev',
+  author: 'betsy
 
   // open graph metadata (optional)
   description: '2skydev blog - developer blog',
